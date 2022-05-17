fdc98031e24a6082638499a7a122d728d62baa32
David Castro <sistemas@impocali.com>
2022-05-17 14:36:27 -0500

    conexion db

--- app.py
+++ app.py
@@ -1,38 +1,19 @@
-#imports
-from flask import Flask, request, make_response
-from flask_sqlalchemy import SQLAlchemy
-import config
-
+from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-
-# Google Cloud SQL (change this accordingly)
-PASSWORD ="1mp0c4l1C5"
-PUBLIC_IP_ADDRESS ="34.138.45.235"
-DBNAME ="comprasic"
-PROJECT_ID ="co-impocali-cld-01"
-INSTANCE_NAME ="bd-compras-ic"
-
-# configuration
-app.config["SECRET_KEY"] = "yoursecretkey"
-app.config["SQLALCHEMY_DATABASE_URI"]= f"mysql + mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket =/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
-
-db = SQLAlchemy(app)
-
+app.config['MYSQL_HOST']='34.138.45.235'
+app.config['MYSQL_USER']='davidc'
+app.config['MYSQL_PASSWORD']='1mp0c4l1C5'
+app.config['MYSQL_DB']='comprasic-test'
+mysql = MySQL(app)
+ 
 @app.route('/')
-def testdb():
-    try:
-        db.session.query(text('1')).from_statement(text('SELECT 1')).all()
-        return '<h1>It works.</h1>'
-    except Exception as e:
-        # e holds description of the error
-        error_text = "<p>The error:<br>" + str(e) + "</p>"
-        hed = '<h1>Something is broken.</h1>'
-        return hed + error_text
+def categorias():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT * FROM categoria''')
+    rv = cur.fetchall()
+    return str(rv)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
-
-def obtener_conexion():
-    return pymysql.connect(host='')
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost', port=5000)
