31d04ca6c1ea277ad7e7c50cb6ee6c4487e2a35e
sebascampo <scampovidal@gmail.com>
2022-05-17 11:57:57 -0500

    3 commit

--- app.py
+++ app.py
@@ -20,12 +20,73 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
 
 db = SQLAlchemy
 
-# User ORM for SQLAlchemy
-@app.route("/add", methods =['POST'])
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key = True, nullable = False)
+    name = db.Column(db.String(50), nullable = False)
+    email = db.Column(db.String(50), nullable = False, unique = True)
+
+@app.route('/add', methods =['POST'])
 def add():
     # getting name and email
-    return "Hello World!"
+    name = request.form.get('name')
+    email = request.form.get('email')
 
+    # checking if user already exists
+    user = Users.query.filter_by(email = email).first()
+
+    if not user:
+        try:
+            # creating Users object
+            user = Users(
+                name = name,
+                email = email
+            )
+            # adding the fields to users table
+            db.session.add(user)
+            db.session.commit()
+            # response
+            responseObject = {
+                'status' : 'success',
+                'message': 'Successfully registered.'
+            }
+
+            return make_response(responseObject, 200)
+        except:
+            responseObject = {
+                'status' : 'fail',
+                'message': 'Some error occured !!'
+            }
+
+            return make_response(responseObject, 400)
+
+    else:
+        # if user already exists then send status as fail
+        responseObject = {
+            'status' : 'fail',
+            'message': 'User already exists !!'
+        }
+
+        return make_response(responseObject, 403)
+
+@app.route('/view')
+def view():
+    # fetches all the users
+    users = Users.query.all()
+    # response list consisting user details
+    response = list()
+
+    for user in users:
+        response.append({
+            "name" : user.name,
+            "email": user.email
+        })
+
+    return make_response({
+        'status' : 'success',
+        'message': response
+    }, 200)
+
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
 
